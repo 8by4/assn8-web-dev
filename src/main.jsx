673ae6c9d8798efbad5ac72d1a63b795f248115b
8by4 <aidanquinn147@gmail.com>
2025-11-14 20:43:49 -0500

    create main components

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+
+import CompThrow from './ComputerThrowComponent.jsx'
+import PlayThrow from './PlayerThrowComponent.jsx'
+import DisplayResult from './DisplayResultComponent.jsx'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    { }
+      <div>
+        <h1>Rock Paper Scissors</h1>
+        <CompThrow />
+        <PlayThrow />
+        <DisplayResult />
+    { }
+      </div>
   </StrictMode>,
 )
